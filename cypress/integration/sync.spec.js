8cc4e3741eadd380baa0fc6be065bf6c0e88a214
Hiago Barbosa <hiaaugusto@hotmail.com>
2020-09-22 23:22:18 -0300

    Update sync.spec.js

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -21,4 +21,26 @@ describe('Esperas...', () => {
         cy.get('#novoCampo')
             .type('Funciona')
     })
+
+    it.only('Deve fazer retrys', () => {
+        cy.get('#buttonDelay')
+            .click()
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('Funciona')
+    })
+
+    it.only('Uso do fins', () => {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+
+        //cy.get('#lista li')
+          //  .find('span')
+           // .should('contain', 'Item 2')
+
+        cy.get('#lista li span')
+           .should('contain', 'Item 2')
+    })
 })
