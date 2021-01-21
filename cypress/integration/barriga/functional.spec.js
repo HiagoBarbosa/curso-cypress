c555ea543b5bcc0c81d2af21620ace02fa7b32ec
Hiago Barbosa <56732129+HiagoBarbosa@users.noreply.github.com>
2021-01-21 15:14:10 -0300

    commit

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -16,11 +16,11 @@ describe('Work with alerts', () => {
         cy.get(loc.MASSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
-    it.only('Should update an account', () => {
+    it('Should update an account', () => {
         cy.get(loc.MENU.SETTINGS).click()
         cy.get(loc.MENU.CONTAS).click()
         //cy.get(':nth-child(7) > :nth-child(2) > .fa-edit')
-        cy.xpach(loc.CONTAS.XP_BTN_ALTERAR)
+        cy.xpach(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta de teste')).click()
         cy.get(loc.CONTAS.NOME).clear().type('Conta alterada')
         cy.get(loc.CONTAS.BTN_SAVE).click()
         cy.get(loc.MASSAGE).should('contain', 'Conta alterada com sucesso')
@@ -31,5 +31,31 @@ describe('Work with alerts', () => {
         cy.get(loc.CONTAS.NOME).type('Conta Alterada')
         cy.get(loc.CONTAS.BTN_SAVE).click()
         cy.get(loc.MASSAGE).should('contain','code 400')
+    })
+
+    it('Should create a transaction', () => {
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta Alterada')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.EXTRATO.LINHA).should('have.length', 7)
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist')
+    })
+
+    it('Should get balance', () => {
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta Alterada')).should('contain', 123,00)
+    })
+
+    it('Should remova a transaction', () => {
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.get(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Desc')).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
     })
 })
